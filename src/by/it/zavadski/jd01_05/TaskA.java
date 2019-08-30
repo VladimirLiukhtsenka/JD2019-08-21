66295c8222d973c2dd88a6376d8d4c2a0decd465
Eugene Zavadski <zavadski.er@gmail.com>
2019-08-30 18:38:26 +0300

    Tasks 1-4.

--- src/by/it/zavadski/jd01_05/TaskA.java
+++ src/by/it/zavadski/jd01_05/TaskA.java
@@ -6,12 +6,12 @@ public class TaskA{
     public static void main(String[] args) {
         step3();
     }
-    public static void void step3()
+    public static void step3()
     {
         double x=12.1;
         for (double a = -5; a <=12 ; a+=3.75) {
             double f=exp(a+x) - 3.45*a;
-            System.out.println("For a=%12g f=%12g",a,f);
+            System.out.printf("For a=%12g f=%12g",a,f);
         }
     }
 
