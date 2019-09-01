07af0c8f5c15c31e333da2a595d82392f0abcdfa
Vladimir-x <vladimirstrong6@gmail.com>
2019-09-01 20:35:12 +0300

    in the process

---
+++ src/by/it/gorevoy/jd01_02/TaskC.java
@@ -0,0 +1,27 @@
+package by.it.gorevoy.jd01_02;
+
+import java.util.Scanner;
+public class TaskC {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+
+
+
+
+
+    }
+
+    private static void step1(){
+
+    }
+    private static void step2(){
+
+    }
+    private static void step3(){
+
+    }
+
+}
+
