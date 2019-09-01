4ba9bd3d4e71df0c32f0568b18e7c74ad7b03a9a
Vladimir-x <vladimirstrong6@gmail.com>
2019-09-01 20:35:12 +0300

    in the process

---
+++ src/by/it/gorevoy/jd01_05/TaskB.java
@@ -0,0 +1,20 @@
+package by.it.gorevoy.jd01_05;
+
+import static java.lang.Math.*;
+
+public class TaskB {
+    public static void main(String[] args) {
+        step1();
+
+    }
+
+    private static void step1() {
+        for (double a = 0; a <= 2; a += 0.2) {
+            double y = 0;
+            for (int x = 1; x <= 6; x++) {
+                y+=pow(7,a)-cos(x);
+            }
+            System.out.printf("a=%.2f y =%.4f \n", a, y);
+        }
+    }
+}
