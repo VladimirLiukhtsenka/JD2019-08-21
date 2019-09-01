c02d3d32eeffcbb459f092c6b76f6c93a1355032
Eugene Zavadski <zavadski.er@gmail.com>
2019-09-02 00:27:41 +0300

    Tasks B2 for jf01_05.

--- src/by/it/zavadski/jd01_05/TaskB.java
+++ src/by/it/zavadski/jd01_05/TaskB.java
@@ -8,18 +8,45 @@ public class TaskB {
     }
 
     private static void step4() {
-double y=0;
-
-        for (double a = 0; a <=2 ; a+=0.2) {
-            for (double x = 1; x <6 ; x++) {
-                y=y+(pow(7,a))-cos(x);
-
-            }
-
-            System.out.printf("For a=%.2f Sum=%f\n",a,y);
+            double y=0;
+        for (double a = 0; a <=2; a=a+0.2){
+        for (int x = 1; x <=6; x++) {
+            y+= (pow(7, a)-cos(x));
+        }
+            System.out.printf("For a=%.2f Sum=%g\n", a, y);
         }
     }
 
     private static void step5() {
+        double beta=0;
+        double x=0;
+        boolean solutionExist;
+
+        for ( x =-5.5; x <2 ; x+=0.5) {
+            if ((x / 2) > -2 && ((x / 2) <= -1)) {
+                    beta = sin(x * x);
+            solutionExist = true;
+        }
+            else if ((x/2)>-1&&((x/2)<0.2)) {
+                beta = cos(x * x);
+                solutionExist = true;
+            }
+            else if((x/2)==0.2) {
+                beta = 1 / tan(x * x);
+                solutionExist = true;
+            }
+            else {
+                solutionExist=false;
+            }
+
+            if (solutionExist==true){
+                double alpha=log10(abs(beta+2.74));
+                System.out.printf("При x/2=%3.2f alpha=%g\n",x / 2,alpha);
+            }
+            else {
+                System.out.printf("При x/2=%3.2f вычисления не определены\n",x/2);
+            }
+        }
+
     }
 }
