7494b64604f9e70446d221f789f851c0721f091c
akhremYuri <akhremr@gmail.com>
2019-09-05 10:58:57 +0300

    Task jd01_07

---
+++ src/by/it/akhrem/jd01_07/Scalar.java
@@ -0,0 +1,23 @@
+package by.it.akhrem.jd01_07;
+
+public class Scalar extends Var {
+
+    private double value;
+
+    public Scalar(double value) {
+        this.value = value;
+    }
+
+    public Scalar(Scalar otherScalar) {
+        value = otherScalar.value;
+    }
+
+    public Scalar(String stringValue) {
+        value = Double.valueOf(stringValue);
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(value);
+    }
+}
