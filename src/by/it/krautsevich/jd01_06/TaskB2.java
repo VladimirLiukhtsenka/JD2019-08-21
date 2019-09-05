efa4d9e6e6c3eabd9423f7cf82aa41c9614fc71a
krautsevich <mihail4egevara@gmail.com>
2019-09-05 23:03:45 +0200

    Task6 B2

--- src/by/it/krautsevich/jd01_06/TaskB2.java
+++ src/by/it/krautsevich/jd01_06/TaskB2.java
@@ -1,4 +1,31 @@
 package by.it.krautsevich.jd01_06;
 
 public class TaskB2 {
-}
+    public static void main(String[] args) {
+
+    TaskB();
+    }
+
+    private static void TaskB() {
+        String poem1 = Poem.text;
+        String [] poem = poem1.split("[.!?]");
+        for (int i = 0; i < poem.length; i++) { poem[i] = poem[i].trim();}
+
+
+        String forSort = poem[0];
+        for (int numberOfSort = 0; numberOfSort < poem.length - 1; numberOfSort++)
+            for (int i = 0; i < poem.length - 1; i++) {
+                if (poem[i].length() > poem[i + 1].length()) {
+                    forSort = poem[i + 1];
+                    poem[i + 1] = poem[i];
+                    poem[i] = forSort;}}
+        for (int i = 0; i < poem.length; i++) {
+            System.out.println(i);
+        }
+
+
+
+
+
+    }}
+
