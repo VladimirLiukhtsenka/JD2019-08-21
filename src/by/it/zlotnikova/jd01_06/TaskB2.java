f5e542871db613282a4c8ef8a50bd4f433308e7c
Ekaterina Zlotnikova <e.zlotnikova@gmail.com>
2019-09-05 19:36:56 +0300

    [ezlotnikova] Lesson tasks 6 A and B1

---
+++ src/by/it/zlotnikova/jd01_06/TaskB2.java
@@ -0,0 +1,26 @@
+package by.it.zlotnikova.jd01_06;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class TaskB2 {
+    public static void main(String[] args) {
+        String poem = Poem.text;
+        poem = poem.replaceAll("\\.\\.\\.", ""); //suits for this particular case only
+        String[] sentences = poem.split("\\.|\\?|!");
+
+        for (int i = 0; i < sentences.length; i++) {
+            sentences[i] = sentences[i].replaceAll("[^а-яА-ЯёЁ]+", " ");
+            sentences[i] = sentences[i].trim();
+        }
+
+//        int comp = Comparator.comparing(sentences[0], sentences[1])
+//        System.out.println(comp);
+        Arrays.sort(sentences);
+
+        for (String sentence : sentences) {
+            System.out.println(sentence);
+        }
+    }
+}
