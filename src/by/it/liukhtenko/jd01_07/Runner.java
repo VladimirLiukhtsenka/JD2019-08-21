9e6e282a26ee5fbe12dfd55a55d2cb6e82a14571
VladimirLiukhtsenka <liukhtenko.v@mail.ru>
2019-09-05 23:49:51 +0300

    Task 01_06 B Task 01_07 A,B,C

---
+++ src/by/it/liukhtenko/jd01_07/Runner.java
@@ -0,0 +1,16 @@
+package by.it.liukhtenko.jd01_07;
+
+import java.util.Arrays;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var v1 = new Scalar( "1.25");
+        Var v2 = new Vector("{1.0, 2.0, 4.0}");
+        System.out.println(v1);
+        System.out.println(v2);
+
+        String s = "{{1,2,2},{3,4,2}}";
+;       Var v3 = new Matrix(s);
+        System.out.println(v3);
+    }
+}
