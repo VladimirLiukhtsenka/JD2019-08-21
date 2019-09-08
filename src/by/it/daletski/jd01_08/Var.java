767f1f5b0799097c2011f7fb5061935a72c4c226
Wladyslaw <vladaletsky@gmail.com>
2019-09-08 22:02:28 +0300

    jd01_07 (A-B1) complete

--- src/by/it/daletski/jd01_08/Var.java
+++ src/by/it/daletski/jd01_08/Var.java
@@ -5,25 +5,25 @@ package by.it.daletski.jd01_08;
 
      @Override
      public Var add(Var other) {
-         System.out.println ("Операция сложения"+this+"+"+other+"невозможна");
+         System.out.println ("Операция сложения"+this+"+"+other+" невозможна");
          return null;
      }
 
      @Override
      public Var sub(Var other) {
-         System.out.println ("Операция вычитания"+this+"-"+other+"невозможна");
+         System.out.println ("Операция вычитания"+this+"-"+other+" невозможна");
          return null;
      }
 
      @Override
      public Var mul(Var other) {
-         System.out.println ("Операция умножения"+this+"*"+other+"невозможна");
+         System.out.println ("Операция умножения"+this+"*"+other+" невозможна");
          return null;
      }
 
      @Override
      public Var div(Var other) {
-         System.out.println ("Операция деления"+this+"/"+other+"невозможна");
+         System.out.println ("Операция деления"+this+"/"+other+" невозможна");
          return null;
      }
 
--- src/by/it/daletski/jd01_08/Vector.java
+++ src/by/it/daletski/jd01_08/Vector.java
@@ -6,29 +6,47 @@ class Vector extends Var {
 
     private double[] value;
 
-    Vector(double[] value){
+    Vector(double[] value) {
         this.value = Arrays.copyOf (value, value.length);
     }
 
     public Vector(Vector vector) {
-        this.value = Arrays.copyOf(vector.value, vector.value.length);
+        this.value = Arrays.copyOf (vector.value, vector.value.length);
     }
 
     public Vector(String strVector) {
-        String[] strArray = strVector.replaceAll("\\{", "").replaceAll("}", "").split(",");
+        String[] strArray = strVector.replaceAll ("\\{", "").replaceAll ("}", "").split (",");
         value = new double[strArray.length];
         for (int i = 0; i < strArray.length; i++) {
-            value[i] = Double.parseDouble(strArray[i]);
+            value[i] = Double.parseDouble (strArray[i]);
         }
     }
 
     @Override
-    public String toString(){
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double[] res = Arrays.copyOf (value, value.length);
+            for (int i = 0; i < res.length; i++) {
+                res[i] = res[i] + ((Scalar) other).getValue ();
+            }
+            return new Vector (res);
+        } else if (other instanceof Vector) {
+            double[] res = Arrays.copyOf (value, value.length);
+            for (int i = 0; i < res.length; i++) {
+                res[i] = res[i] + ((Vector) other).value[i];
+            }
+            return new Vector (res);
+        } else
+            return super.add (other);
+    }
+
+    @Override
+    public String toString() {
         StringBuilder sb = new StringBuilder ("{");
         String delimetr = "";
-        for (double element : value){
+        for (double element : value) {
             sb.append (delimetr).append (element);
-            delimetr=", ";
+            delimetr = ", ";
         }
         sb.append ("}");
         return sb.toString ();
