fbe289fe4844abc4754f4537f231a0867a0cf7ae
Ekaterina Zlotnikova <e.zlotnikova@gmail.com>
2019-09-08 22:49:31 +0300

    [ezlotnikova] Lesson 6 tasks B2 and C1

--- src/by/it/zlotnikova/jd01_06/TaskB2.java
+++ src/by/it/zlotnikova/jd01_06/TaskB2.java
@@ -1,11 +1,12 @@
 package by.it.zlotnikova.jd01_06;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TaskB2 {
     public static void main(String[] args) {
         String poem = Poem.text;
-        poem = poem.replaceAll("\\.\\.\\.", ""); //suits for this particular case only
+        poem = poem.replaceAll("\\.\\.\\.", "~");
         String[] sentences = poem.split("\\.|\\?|!");
 
         for (int i = 0; i < sentences.length; i++) {
@@ -13,9 +14,12 @@ public class TaskB2 {
             sentences[i] = sentences[i].trim();
         }
 
-//        int comp = Comparator.comparing(sentences[0], sentences[1])
-//        System.out.println(comp);
-        Arrays.sort(sentences);
+        Arrays.sort(sentences, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return Integer.compare(o1.length(), o2.length());
+            }
+        });
 
         for (String sentence : sentences) {
             System.out.println(sentence);
