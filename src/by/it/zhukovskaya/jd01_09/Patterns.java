04c6983805bb96dc588a6ee87524a04ed55cf935
zhukovskaya1606 <zhukovskaya1606@gmail.com>
2019-09-11 10:24:59 +0300

    Tasks9

---
+++ src/by/it/zhukovskaya/jd01_09/Patterns.java
@@ -0,0 +1,11 @@
+package by.it.zhukovskaya.jd01_09;
+
+public class Patterns {
+    static final String OPERATION = "[-+*/]";
+    static final String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+    static final String VECTOR = "\\{" + SCALAR + "(," + SCALAR + ")*}";
+    static final String MATRIX = "\\{" + VECTOR + "(," + VECTOR + ")*}";
+
+    private Patterns() {
+    }
+}
