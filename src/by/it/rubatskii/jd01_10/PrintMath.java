7bd0c018099ce9d1dc644cd8de7676af64f12d5c
Uspech <uda4nii.ar@gmail.com>
2019-09-12 21:26:33 +0300

    jd10B+ Complete

---
+++ src/by/it/rubatskii/jd01_10/PrintMath.java
@@ -0,0 +1,44 @@
+package by.it.rubatskii.jd01_10;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class PrintMath {
+    public static void main(String[] args) {
+        Class<Math> mathClass = Math.class;
+        Method[] methods = mathClass.getMethods();
+        for (Method method : methods) {
+            StringBuilder line = new StringBuilder();
+            int modifiers = method.getModifiers();
+            if (Modifier.isPublic(modifiers)) line.append("public ");
+            if (Modifier.isPublic(modifiers)) line.append("static ");
+            line.append(method.getReturnType().getSimpleName()).append(" ");
+            line.append(method.getName()).append('(');
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            for (Class<?> parameterType : parameterTypes) {
+                line.append(parameterType.getSimpleName()).append(',');
+
+            }
+
+            if (parameterTypes.length >= 1) {
+                line.setLength(line.length() - 1);
+            }
+            line.append(')');
+            System.out.println(line);
+
+        }
+        Field[] fields = mathClass.getDeclaredFields();
+        for (Field field : fields) {
+            StringBuilder line = new StringBuilder();
+            int modifiers = field.getModifiers();
+            if (Modifier.isPublic(modifiers) && Modifier.isStatic(modifiers)) {
+                line.append(field.getType().getSimpleName()).append(" ");
+
+                line.append(field.getName());
+                System.out.println(line);
+            }
+        }
+    }
+}
+
