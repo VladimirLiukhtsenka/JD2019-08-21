5e995637aa5846c11b28066141a2d064adf7bcb3
Eugene Zavadski <zavadski.er@gmail.com>
2019-09-12 18:16:40 +0300

    jd01_10 A, B is completed. jd01_08: files Matrix and Vector are updated.

--- src/by/it/zavadski/jd01_10/PrintMath.java
+++ src/by/it/zavadski/jd01_10/PrintMath.java
@@ -1,5 +1,8 @@
 package by.it.zavadski.jd01_10;
 
+
+
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
@@ -8,19 +11,15 @@ public class PrintMath {
         Class<Math> mathClass = Math.class;
         Method[] methods = mathClass.getDeclaredMethods();
         for (Method method : methods) {
-            StringBuilder line=new StringBuilder();
-            int modifiers=method.getModifiers();
-             if (Modifier.isPublic(modifiers)) line.append("public ");
-             if (Modifier.isStatic(modifiers)) line.append("static ");
-
-             line.append(method.getName()).append('(');
-
-            Class<?>[] parameterTypes=method.getParameterTypes();
-            for (Class<?> parameterType:parameterTypes) {
-                line.append(parameterType.getSimpleName()).append(',');
+          if ((method.getModifiers() & Modifier.STATIC)==Modifier.STATIC){ //Ensure that modifier for method is static
+              if ((method.getModifiers()& Modifier.PUBLIC)==Modifier.PUBLIC) { //Ensure that modifier for method is public
+                    System.out.println(String.valueOf(method).replace("java.lang.Math.",""));
+                }
+            }
         }
-            line.append(')');
-            System.out.println(line);
+        Field[] fields=mathClass.getFields();
+        for(Field field:fields){
+            System.out.println(String.valueOf(field).replace("java.lang.Math.",""));
         }
     }
 }
--- src/by/it/zavadski/jd01_10/PrintString.java
+++ src/by/it/zavadski/jd01_10/PrintString.java
@@ -1,4 +1,19 @@
 package by.it.zavadski.jd01_10;
 
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
 public class PrintString {
+    public static void main(String[] args) {
+    Class<String> string=String.class;
+    Method[] methods=string.getDeclaredMethods();
+        for (Method method : methods) {
+            if((method.getModifiers()& Modifier.STATIC)!=Modifier.STATIC){
+                System.out.println(String.valueOf(method).replace("java.lang.Math.",""));
+
+            }
+        }
+
+    }
 }
