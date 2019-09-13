cead3f7422fb74a4292403c324f6a801436fa900
loktev <loktev@tut.by>
2019-09-13 12:34:32 +0300

    classwork 11 Task A

---
+++ src/by/it/akhmelev/calc/Parser.java
@@ -0,0 +1,39 @@
+package by.it.akhmelev.calc;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Parser {
+
+    Var calc(String expression) {
+        expression = expression.replace(" ", "");
+        String[] part = expression.split(Patterns.OPERATION, 2);
+        if (part.length == 1)
+            return Var.createVar(part[0]);
+        //A=7
+        Var two = Var.createVar(part[1]);
+        Pattern patternOperation = Pattern.compile(Patterns.OPERATION);
+        Matcher matcher = patternOperation.matcher(expression);
+        String operation="";
+        if (matcher.find()) operation = matcher.group();
+
+        if (operation.equals("=")) {
+            Var.save(part[0], two);
+            return two;
+        }
+        Var one = Var.createVar(part[0]);
+        //add exceptions
+        switch (operation) {
+            case "+":
+                return one.add(two);
+            case "-":
+                return one.sub(two);
+            case "*":
+                return one.mul(two);
+            case "/":
+                return one.div(two);
+        }
+        return null;
+    }
+
+}
