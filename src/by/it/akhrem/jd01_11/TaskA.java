e5a1b38fde2958d8aa6c79e6b4f0fe06cbde9f18
akhremYuri <akhremr@gmail.com>
2019-09-13 16:27:40 +0300

    Task jd01_09

---
+++ src/by/it/akhrem/jd01_11/TaskA.java
@@ -0,0 +1,17 @@
+package by.it.akhrem.jd01_11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskA {
+    public static void main(String[] args) {
+        List<String> list=  new ArrayList<>();
+        list.add("Hello1");
+        list.add("Hello2");
+        list.add("Hello3");
+        list.add("Hello4");
+        list.add("Hello5");
+        list.add("Hello6");
+        System.out.println(list);
+    }
+}
