bf7d708c9e3d95b6fe2c53e4018d745b4a973edd
Vladimir-x <vladimirstrong6@gmail.com>
2019-09-15 11:42:13 +0300

    jd01_10 = Full.Test

---
+++ src/by/it/gorevoy/jd01_10/BeanTester.java
@@ -0,0 +1,22 @@
+package by.it.gorevoy.jd01_10;
+
+import java.lang.reflect.Method;
+
+public class BeanTester {
+    public static void main(String[] args) throws Exception {
+        Class<?> beanClass = Bean.class;
+        Class<Param> paramClass = Param.class;
+        Method[] methods = beanClass.getDeclaredMethods();
+        for (Method method : methods)
+            if (method.isAnnotationPresent(paramClass)) {
+                Param annotation = method.getAnnotation(paramClass);
+                int aParam = annotation.a();
+                int bParam = annotation.b();
+                Object invokeObject = method.invoke(beanClass.getConstructor().newInstance(), aParam, bParam);
+                System.out.printf("%s %s\n", method.getName(), invokeObject.toString());
+            }
+    }
+
+    public BeanTester() {
+    }
+}
