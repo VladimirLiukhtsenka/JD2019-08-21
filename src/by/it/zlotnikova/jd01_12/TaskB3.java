1cc26d993f6961de078ff03ce2e8714faeba7ad0
Ekaterina Zlotnikova <e.zlotnikova@gmail.com>
2019-09-17 22:50:10 +0300

    [ezlotnikova] Lesson 12 Task B3 +

--- src/by/it/zlotnikova/jd01_12/TaskB3.java
+++ src/by/it/zlotnikova/jd01_12/TaskB3.java
@@ -12,25 +12,37 @@ public class TaskB3 {
             arrPeoples.add(String.valueOf(rnd.nextInt(100)));
         }
         LinkedList<String> linkPeoples = new LinkedList<>(arrPeoples);
+        LinkedList<String> linkPeoples2 = new LinkedList<>(arrPeoples);
 
+//        System.out.println(arrPeoples);
         long initTime1 = System.currentTimeMillis();
         String name1 = process(arrPeoples);
         long time1 = System.currentTimeMillis() - initTime1;
         System.out.println("Time for ArrayList - " + time1 + "ms");
+        System.out.println(name1);
 
+//        System.out.println(linkPeoples);
         long initTime2 = System.currentTimeMillis();
         String name2 = process(linkPeoples);
         long time2 = System.currentTimeMillis() - initTime2;
         System.out.println("Time for LinkedList - " + time2 + "ms");
+        System.out.println(name2);
+
+//        System.out.println(linkPeoples2);
+        long initTime3 = System.currentTimeMillis();
+        String name3 = processOther(linkPeoples2);
+        long time3 = System.currentTimeMillis() - initTime3;
+        System.out.println("Time for Queue - " + time3 + "ms");
+        System.out.println(name3);
     }
 
     private static String process(ArrayList<String> peoples) {
-        int count = 0;
+        int count = 1;
         while (peoples.size() > 1) {
             Iterator<String> it = peoples.iterator();
             while (it.hasNext()) {
                 it.next();
-                if (count % 2 != 0) {
+                if (count % 2 == 0) {
                     it.remove();
                 }
                 count++;
@@ -40,17 +52,31 @@ public class TaskB3 {
     }
 
     private static String process(LinkedList<String> peoples) {
-        int count = 0;
+        int count = 1;
         while (peoples.size() > 1) {
             Iterator<String> it = peoples.iterator();
             while (it.hasNext()) {
                 it.next();
-                if (count % 2 != 0) {
+                if (count % 2 == 0) {
                     it.remove();
                 }
                 count++;
             }
         }
         return peoples.get(0);
+    }
+
+    private static String processOther(LinkedList<String> peoples) {
+        Queue<String> queue = peoples;
+        int count = 1;
+        String element;
+        while (queue.size() > 1) {
+            element = queue.remove();
+            if (count % 2 != 0) {
+            queue.add(element);
+            }
+            count++;
+        }
+        return queue.element();
     }
 }
