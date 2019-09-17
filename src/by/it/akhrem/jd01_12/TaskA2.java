f602c1d044e53ee4c63b5d8b9095a36e19c5c271
akhremYuri <akhremr@gmail.com>
2019-09-17 13:12:41 +0300

    Task jd01_12

--- src/by/it/akhrem/jd01_12/TaskA2.java
+++ src/by/it/akhrem/jd01_12/TaskA2.java
@@ -6,11 +6,12 @@ import java.util.Set;
 
 public class TaskA2 {
     public static void main(String[] args) {
-        Integer[] arrayA = {1, 1, 2,2,3,4,5,6,6};
-        Integer[] arrayB= {4,4,4,5,5,6,6,7,8,8, 9,9, 9};
+        Integer[] arrayA = {1, 1, 2, 2, 3, 4, 5, 6, 6};
+        Integer[] arrayB = {4, 4, 4, 5, 5, 6, 6, 7, 8, 8, 9, 9, 9};
         Set<Integer> setA = new HashSet<>(Arrays.asList(arrayA));
         Set<Integer> setB = new HashSet<>(Arrays.asList(arrayB));
         System.out.println(getUnion(setA, setB));
+        System.out.println(getCross(setA, setB));
     }
 
     private static Set<Integer> getCross(Set<Integer> a, Set<Integer> b) {
@@ -21,7 +22,6 @@ public class TaskA2 {
 
     private static Set<Integer> getUnion(Set<Integer> a, Set<Integer> b) {
         Set<Integer> result = new HashSet<>(a);
-        //result.addAll(a);
         result.addAll(b);
         return result;
 
