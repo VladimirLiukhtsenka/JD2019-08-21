235dc998468813c6874a26eef27260c9d464a14e
Ekaterina Zlotnikova <e.zlotnikova@gmail.com>
2019-09-17 21:10:43 +0300

    [ezlotnikova] Lesson 12 Task B2

---
+++ src/by/it/zlotnikova/jd01_12/TaskB2.java
@@ -0,0 +1,52 @@
+package by.it.zlotnikova.jd01_12;
+
+import java.util.*;
+
+public class TaskB2 {
+
+    public static void main(String[] args) {
+        ArrayList<String> arrPeoples = new ArrayList<>(10);
+        Random rnd = new Random();
+        for (int i = 0; i < 10; i++) {
+            arrPeoples.add(String.valueOf(rnd.nextInt(10) + 1));
+        }
+        LinkedList<String> linkPeoples = new LinkedList<>(arrPeoples);
+        System.out.println(arrPeoples);
+        String name1 = process(arrPeoples);
+        System.out.println(name1);
+
+        System.out.println(linkPeoples);
+        String name2 = process(linkPeoples);
+        System.out.println(name2);
+    }
+
+    private static String process(ArrayList<String> peoples) {
+        int count = 0;
+        while (peoples.size() > 1) {
+            Iterator<String> it = peoples.iterator();
+            while (it.hasNext()) {
+                it.next();
+                if (count % 2 != 0) {
+                    it.remove();
+                }
+                count++;
+            }
+        }
+        return peoples.get(0);
+    }
+
+    private static String process(LinkedList<String> peoples) {
+        int count = 0;
+        while (peoples.size() > 1) {
+            Iterator<String> it = peoples.iterator();
+            while (it.hasNext()) {
+                it.next();
+                if (count % 2 != 0) {
+                    it.remove();
+                }
+                count++;
+            }
+        }
+        return peoples.get(0);
+    }
+}
