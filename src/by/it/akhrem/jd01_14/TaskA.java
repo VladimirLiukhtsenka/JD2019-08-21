ecf94ff68eec0e4d5e55cb302be3c4e7d62b5757
akhremYuri <akhremr@gmail.com>
2019-09-21 12:01:52 +0300

    Task jd01_14

--- src/by/it/akhrem/jd01_14/TaskA.java
+++ src/by/it/akhrem/jd01_14/TaskA.java
@@ -6,14 +6,45 @@ import java.util.List;
 
 public class TaskA {
     public static void main(String[] args) {
-        /*Class<TaskA> taskAClass = TaskA.class;
+        Class<TaskA> taskAClass = TaskA.class;
         String path = getPath(taskAClass);
         String filename = path + "dataTaskA.bin";
-        //write Binary
+        writeBinary(filename);
+        List<Integer> list = new ArrayList<>(20);
+        readBinary(filename, list);
+        String fileTxt = path + "resultTaskA.txt";
+        try (PrintWriter out2 = new PrintWriter(new FileWriter(fileTxt))){
+            double sum = 0;
+            for (Integer i : list) {
+                System.out.printf("%d ", i);
+                out2.print(i+" ");
+                sum += i;
+            }
+            System.out.print("\navg=" + sum/list.size());
+            out2.print("\navg=" + sum/list.size());
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void readBinary(String filename, List<Integer>list) {
+        try (DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));) {
+            while (dis.available() > 0) {
+                int i = dis.readInt();
+                list.add(i);
+            }
+        } catch (IOException e) {
+            System.err.println("Error read " + filename);
+        }
+    }
+
+    private static void writeBinary(String filename) {
         try(
-        FileOutputStream fos = new FileOutputStream(filename);
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        DataOutputStream dos = new DataOutputStream(bos);
+                FileOutputStream fos = new FileOutputStream(filename);
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                DataOutputStream dos = new DataOutputStream(bos);
         ) {
             for (int i = 0; i < 20; i++){
                 int value = (int)(Math.random()*100);
@@ -22,37 +53,6 @@ public class TaskA {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // read Binary
-        List<Integer> list = new ArrayList<>(20);
-        try (DataInputStream dis = new DataInputStream(new BufferedInputStream(new FileInputStream(filename)));) {
-            while (dis.available() > 0) {
-                int i = dis.readInt();
-                list.add(i);
-
-            }
-        } catch (IOException e) {
-            System.err.println("Error read " + filename);
-        }
-        double sum = 0;
-        for (Integer i : list) {
-            System.out.printf("%d ", i);
-            sum += i;
-        }
-        System.out.printf("\navg=%f\n", sum/list.size());
-
-        String fileTxt = path + "resultTaskA.txt";
-        try {
-            PrintWriter out = new PrintWriter(new FileWriter(fileTxt));
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        double sum = 0;
-        for (Integer i : list) {
-            System.out.printf("%d ", i);
-            sum += i;
-        }
-        System.out.printf("\navg=%f\n", sum/list.size()); */
     }
 
     private static String getPath(Class<TaskA> taskAClass) {
---
+++ src/by/it/akhrem/jd01_14/TaskB.java
@@ -0,0 +1,39 @@
+package by.it.akhrem.jd01_14;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TaskB {
+    public static void main(String[] args) {
+        String filename = getPath(TaskB.class) + "text.txt";
+        String line = null;
+        int wordCnt = 0;
+        int punctCnt = 0;
+        String fileTxt = getPath(TaskB.class) + "resultTaskB.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(filename));
+             PrintWriter out2 = new PrintWriter(new FileWriter(fileTxt));) {
+            Pattern ptWords = Pattern.compile("[а-яА-ЯёЁ]+");
+            Pattern ptPunctuation = Pattern.compile("[-,.!:]+");
+            while ((line = br.readLine()) != null) {
+                Matcher mtWords = ptWords.matcher(line);
+                while (mtWords.find())
+                    wordCnt++;
+                Matcher mtPunctuation = ptPunctuation.matcher(line);
+                while (mtPunctuation.find())
+                    punctCnt++;
+            }
+            System.out.printf("words=%d , punctuation marks=%d", wordCnt, punctCnt);
+            out2.print("words="+wordCnt+ " , punctuation marks="+punctCnt);
+        } catch (IOException e) {
+            System.err.println("Error read " + filename);
+        }
+    }
+
+    private static String getPath(Class<TaskB> taskBClass) {
+        String rootProject = System.getProperty("user.dir");
+        String subPath = taskBClass.getName().replace(".", File.separator).replace(taskBClass.getSimpleName(), "");
+        String path = rootProject + File.separator + "src" + File.separator + subPath;
+        return path;
+    }
+}
