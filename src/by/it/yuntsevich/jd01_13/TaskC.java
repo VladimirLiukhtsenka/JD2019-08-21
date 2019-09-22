10e0804e10f01de380ec697311e9d1d81a125a0b
Svetlana <svetlana.yuntsevich@gmail.com>
2019-09-22 22:58:25 +0300

    Lesson 14 A,B,C

--- src/by/it/yuntsevich/jd01_13/TaskC.java
+++ src/by/it/yuntsevich/jd01_13/TaskC.java
@@ -5,14 +5,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TaskC {
-    public static void main(String[] args) throws InterruptedException {
+    static Scanner sc;
 
+    public static void main(String[] args) throws InterruptedException {
+         sc = new Scanner(System.in);
         readData();
     }
 
     private static void readData() throws InterruptedException {
-        Scanner sc = new Scanner(System.in);
-
         List<Double> list = new ArrayList<>();
         int counter = 0;
         for (; ; ) {
@@ -23,9 +23,7 @@ public class TaskC {
                 list.add(value);
             }
             catch (Exception e) {
-
                     Thread.sleep(100);
-                   // Collections.reverse(list);
                     for (int i=list.size()-1;i>=0; i--) {
                         System.out.print(list.get(i) + " ");
                     }
