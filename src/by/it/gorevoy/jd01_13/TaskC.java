9836f76c8d096b4d1b74e292439c85764d8b05f2
Vladimir-x <vladimirstrong6@gmail.com>
2019-09-29 20:29:06 +0300

    jd01_13 = Full.

--- src/by/it/gorevoy/jd01_13/TaskC.java
+++ src/by/it/gorevoy/jd01_13/TaskC.java
@@ -1,49 +1,40 @@
 package by.it.gorevoy.jd01_13;
 
-
-import java.util.*;
-
-import static java.util.Collections.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class TaskC {
-    /**
-     * TaskC. Нужно написать в TaskC программу, которая будет вводить вещественные числа с
-     * консоли, после каждой ошибки программа делает паузу в 100 мс и выводит накопленные числа в
-     * обратном порядке.
-     *  Используйте Scanner, но он должен создаваться только один раз и обязательно в методе main.
-     *  Код по чтению чисел с клавиатуры должен быть в методе static void readData().
-     *  Если пользователь ввёл какой-то текст, вместо ввода числа, то метод readData() должен перехватить
-     * исключение, сделать паузу Thread.sleep(100) и вывести на экран все ранее введенные числа.
-     *  Числа нужно выводить без форматирования, через пробел, в порядке обратном вводу.
-     *  После 5 допущенных ошибок программа должна завершиться, пробрасывая ошибку в JVM.
-     */
-    private static int errorCount=0;
-    private static String input;
-    private static List<Double> list= new ArrayList<>();
 
-    public static void main(String[] args) throws Exception {
-        Scanner scanner=new Scanner(System.in);
-        while(errorCount!=5){
-            input=scanner.nextLine();
-         readData();
+    private static Scanner scanner;
+    private static int counter = 0;
+    private static List<Double> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        for(;;) {
+            readData();
         }
     }
-    private static void readData() throws Exception {
-            try {
-                double inputValue=Double.parseDouble(input);
-                list.add(inputValue);
 
-            }
-            catch (NumberFormatException e) {
-                if(errorCount==5)
-                   throw new Exception("Wrong input 5 times",e);
-                else
-                errorCount++;
-                Thread.sleep(100);
-                for (int i = list.size()-1; i >=0 ; i--){
-                    System.out.print(list.get(i)+" ");
+    private static void readData() throws NumberFormatException {
+        String strValue = scanner.next();
+        try {
+            Double newEntry = Double.valueOf(strValue);
+            list.add(newEntry);
+        } catch (NumberFormatException e) {
+            if (counter < 5) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException ex) {
+                    System.err.println(ex.getMessage());
+                }
+                for (int i = list.size() - 1; i >= 0; i--) {
+                    System.out.print(list.get(i) + " ");
                 }
                 System.out.println();
+                counter++;
+            } else throw new NumberFormatException();
+        }
     }
-}}
-
+}
