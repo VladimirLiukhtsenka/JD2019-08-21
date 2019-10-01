27657915b41c77e3e33fc9e8d4b25936ad79cda1
VladimirLiukhtsenka <liukhtenko.v@mail.ru>
2019-10-01 18:43:20 +0300

    02_03 A,B

--- src/by/it/liukhtenko/jd01_01/Main.java
+++ src/by/it/liukhtenko/jd01_01/Main.java
@@ -3,7 +3,7 @@ package by.it.liukhtenko.jd01_01;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	// write your code here
       Hello hello =  new Hello();
       hello.printSlogan();
       hello.setSlogan("Привет мир");
--- src/by/it/liukhtenko/jd01_15/TaskB.java
+++ src/by/it/liukhtenko/jd01_15/TaskB.java
@@ -82,6 +82,7 @@ public class TaskB {
             }
         }catch (IOException ex) {
             ex.printStackTrace();
+
         }
     }
 }
