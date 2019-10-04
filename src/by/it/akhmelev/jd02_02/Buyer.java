9cb2bd80996a3fd9b53044ac2b53ec7073383be6
loktev <loktev@tut.by>
2019-10-04 13:16:49 +0300

    classwork 02_05

--- src/by/it/akhmelev/jd02_02/Buyer.java
+++ src/by/it/akhmelev/jd02_02/Buyer.java
@@ -1,11 +1,19 @@
 package by.it.akhmelev.jd02_02;
 
-class Buyer extends Thread implements IBuyer {
+import java.util.HashSet;
+import java.util.Set;
 
+class Buyer extends Thread implements IBuyer {
 
     Buyer() {
         //important!!!!
         super("Buyer № " + Dispatcher.buyerInMarket());
+    }
+
+    private Set<Good> backet = new HashSet<>();
+
+    public Set<Good> getBacket() {
+        return backet;
     }
 
     @Override
@@ -25,22 +33,26 @@ class Buyer extends Thread implements IBuyer {
     public void chooseGoods() {
         System.out.printf("\t%s started to choose goods\n", this);
         int timeout = Util.random(2000);
+        int count = Util.random(1, 4);
+        while (backet.size() < count) {
+            backet.add(GoodTable.getRandom());
+        }
         Util.sleep(timeout);
         System.out.printf("\t%s end to choose goods\n", this);
     }
 
     @Override
     public void goToQueue() {
-        System.out.println(this+" added to Queue");
-            QueueBuyers.add(this);
-            synchronized (this){
-                try {
-                    this.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+        System.out.println(this + " added to Queue");
+        QueueBuyers.add(this);
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        System.out.println(this+" leave CashDesk");
+        }
+        System.out.println(this + " leave CashDesk");
     }
 
     @Override
