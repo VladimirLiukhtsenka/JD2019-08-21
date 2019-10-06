c4dae19c787a1d777fe1d6159aafad76af4b49d6
Vladimir-x <vladimirstrong6@gmail.com>
2019-10-06 22:49:50 +0300

    jd02_02 not full

---
+++ src/by/it/gorevoy/jd02_02/QueueBuyers.java
@@ -0,0 +1,23 @@
+package by.it.gorevoy.jd02_02;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class QueueBuyers {
+
+    private static BlockingDeque<Buyer> deque = new LinkedBlockingDeque<>(30);
+    //Sync QueueBuyers.class
+    static void add(Buyer buyer){
+        try {
+            deque.putLast(buyer);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Buyer extract() {
+        return deque.pollFirst();
+    }
+
+}
